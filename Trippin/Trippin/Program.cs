4b4165047802e87823373ce2381b86ef6564d6c1
BrunnerAlexander <AlexanderBrunner01@gmail.com>
2019-10-16 23:49:29 +0200

    Solution

---
+++ Trippin/Trippin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace Trippin
+{
+    class Program
+    {
+        private static readonly HttpClient HttpClient = new HttpClient() { BaseAddress = new Uri("https://services.odata.org/TripPinRESTierService/(S(xl0bqnlifipl440ghyxhyvmy))/") };
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var readUsers= await System.IO.File.ReadAllTextAsync("users.json");
+            var users = JsonSerializer.Deserialize<User[]>(readUsers);
+
+            foreach(User user in users)
+            {
+                if (!(await HttpClient.GetAsync(user.UserName)).IsSuccessStatusCode)
+                {
+                    var content = new StringContent(JsonSerializer.Serialize(user), Encoding.UTF8, "application/json");
+                    var userResponse = await HttpClient.PostAsync("People", content);
+                    userResponse.EnsureSuccessStatusCode();
+                    Console.WriteLine("Erstellt");
+                }
+            }
+        }
+    }
+}
---
+++ Trippin/Trippin/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Trippin
+{
+    class User
+    {
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("cityName")]
+        public string CityName { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+    }
+}
